7f62ab515e9ebe9ef5a55673f6ff61124b1c7bb4
nhickman13 <63763990+nhickman13@users.noreply.github.com>
2023-09-13 00:16:54 -0400

    Assingment #2
    Create a Java program that successfully displays in the execution command shell, an ASCII character graphic of the room and painting information as it is specified below. All of the numeric values must be calculated in your code and displayed in the output.
    Learn to declare and assign primitive type variables, cast primitive types, and use the java.lang.Math library. Continue becoming acquainted with the process of developing and testing executable Java code; using of an editor, compiler, and runtime environment as programming tools; and deploying the developed capability on a remote host.

--- Room.java
+++ Room.java
@@ -1,74 +1,114 @@
-package edu.jhupte.cs605201.part1;
+package edu.jhupte.cs605201.part2;
 
-/**
- * Room.java
- *
- * Created: 
- *
- * @author Nickolas A. Hickman
- */
-
-final public class Room {
-    /**
-     * Default constructor, creates a new <code>Room</code> instance.
-     *
-     */
-    public Room() {
+public class Room{
+    /*Inputs string and int. Prints string with int number of spaces from the left edge.*/
+    static void printIndent(String tempString, int colStart){
+        StringBuffer sb = new StringBuffer(tempString);
+        for(int i=1; i<colStart; i++){
+            sb = sb.insert(0," ");
+        }
+        System.out.println(sb);
     }
-    
-    /**
-     * Display room.
-     *
-     * @param args a <code>String[]</code> value
-     */
-    static public void main(String[] args) {
 
-        /*Adjust horizontal position of each element. Enter the column which you would like each element to start in.*/
-        int room_horiz = 6;
-        int key_horiz = 4;
-        int key_elmt_horiz = 6;
-
-        /*Intialize counter and strings to build strings with spaces of lenth corresponding to the desired distance from the left edge*/
-        String rm_spc = "", ky_spc = "", ke_spc = "";
-        int i = 1;
-
-        /*Loops to convert int to string composed of spaces.*/
-        while(i < room_horiz) {
-            rm_spc = rm_spc + " ";
-            i++;
-        }
-
-        i = 1;
-        while(i < key_horiz) {
-            ky_spc = ky_spc + " ";
-            i++;
-        }
-
-        i = 1;
-        while(i < key_elmt_horiz) {
-            ke_spc = ke_spc + " ";
-            i++;
-        }
-
-        /*Print room map with appropriate horizontal position.*/
-        System.out.println(
-                "\n"+
-                        rm_spc+"-------www-------\n"+
-                        rm_spc+"|               |\n"+
-                        rm_spc+"|               |\n"+
-                        rm_spc+"s               w\n"+
-                        rm_spc+"|               w\n"+
-                        rm_spc+"d               w\n"+
-                        rm_spc+"d               |\n"+
-                        rm_spc+"|               o\n"+
-                        rm_spc+"|               |\n"+
-                        rm_spc+"-----www-www-----\n\n"+
-                        ky_spc+"KEY:\n"+
-                        ke_spc+"| vertical wall\n"+
-                        ke_spc+"- horizontal wall\n"+
-                        ke_spc+"d door\n"+
-                        ke_spc+"w window\n"+
-                        ke_spc+"o electrical outlet\n"+
-                        ke_spc+"s electrical switch");
+    static void alignCheck(boolean TEST){
+        if (TEST==true){printIndent("123456789012345678901234567890",0);}
     }
-}// Room
+
+    public static void main(String[] args){
+        final boolean TEST = false;
+
+        /*Declaring spacing from left edge by component.*/
+        final int MAP_SPC = 7;
+        final int KEY_HD_SPC = 4;
+        final int KEY_SPC = 7;
+        final int CALC_HD_SPC = 2;
+        final int CALC_SUBHD_SPC = 4;
+        final int CALC_SPC = 7;
+
+        /*Declaring constants*/
+        final float CEILING_HEIGHT =8.5f;
+        final float DOOR_WIDTH = 3.5f;
+        final float DOOR_HEIGHT = 6.75f;
+        final float WHITE_COST = 24.98f, BLUE_COST = 30.98f;
+        final float TAX_RATIO = 1.06f;
+        final int WALL_1_LENGTH = 17;
+        final int WALL_2_LENGTH = 8;
+        final int WINDOW_WIDTH = 3;
+        final int WINDOW_HEIGHT = 4;
+        final int PRIMER_COATS = 1, COLOR_COATS = 2;
+        final int WHITE_COVERAGE = 500, BLUE_COVERAGE = 400;
+
+
+        /*Declaring variables for calculations*/
+        double ceilingArea, wallArea, windowArea, doorArea;
+        double northArea, southArea, eastArea, westArea, totalWhiteArea, totalBlueArea;
+        double whiteGallons, whiteGallonsPurchase, whiteCost;
+        double blueGallons, blueGallonsPurchase, blueCost, totalCost;
+
+        /*Area algorithms*/
+        ceilingArea = WALL_1_LENGTH * WALL_2_LENGTH;
+        windowArea = WINDOW_HEIGHT * WINDOW_WIDTH;
+        doorArea = DOOR_WIDTH * DOOR_HEIGHT;
+        northArea = WALL_1_LENGTH * CEILING_HEIGHT - windowArea;
+        southArea = WALL_1_LENGTH * CEILING_HEIGHT - (2 * windowArea);
+        eastArea = WALL_2_LENGTH * CEILING_HEIGHT - windowArea;
+        westArea = WALL_2_LENGTH * CEILING_HEIGHT - doorArea;
+        wallArea = northArea + southArea + eastArea + westArea;
+
+        /*Cost algorithms*/
+        totalWhiteArea = COLOR_COATS * ceilingArea + PRIMER_COATS * wallArea;
+        whiteGallons = totalWhiteArea / WHITE_COVERAGE;
+        whiteGallons = Math.round(whiteGallons * 100) / 100.0;
+        whiteGallonsPurchase = Math.ceil(whiteGallons);
+        whiteCost = whiteGallonsPurchase * WHITE_COST;
+        whiteCost = Math.round(whiteCost * 100) / 100.0;
+
+        totalBlueArea = COLOR_COATS * wallArea;
+        blueGallons = totalBlueArea / BLUE_COVERAGE;
+        blueGallons = Math.round(blueGallons *100) / 100.0;
+        blueGallonsPurchase = Math.ceil(blueGallons);
+        blueCost = blueGallonsPurchase * BLUE_COST;
+        blueCost = Math.round(blueCost * 100) / 100.0;
+
+        totalCost = (whiteCost + blueCost) * TAX_RATIO;
+        totalCost = Math.round(totalCost * 100) / 100.0;
+
+        alignCheck(TEST);
+        printIndent("",0);
+        printIndent("-------www-------",MAP_SPC);
+        printIndent("|               |",MAP_SPC);
+        printIndent("|               |",MAP_SPC);
+        printIndent("s               w",MAP_SPC);
+        printIndent("|               w",MAP_SPC);
+        printIndent("d               w",MAP_SPC);
+        printIndent("d               |",MAP_SPC);
+        printIndent("|               o",MAP_SPC);
+        printIndent("|               |",MAP_SPC);
+        printIndent("-----www-www-----",MAP_SPC);
+        printIndent("",0);
+        alignCheck(TEST);
+        printIndent("KEY:                  ",KEY_HD_SPC);
+        printIndent("|   vertical wall     ",KEY_SPC);
+        printIndent("-   horizontal wall   ",KEY_SPC);
+        printIndent("d   door              ",KEY_SPC);
+        printIndent("w   window            ",KEY_SPC);
+        printIndent("o   electrical outlet ",KEY_SPC);
+        printIndent("s   electrical switch ",KEY_SPC);
+        printIndent("===========================",0);
+        alignCheck(TEST);
+        printIndent("ROOM PAINT",CALC_HD_SPC);
+        printIndent("Ceiling white @ $24.98/gallon",CALC_SUBHD_SPC);
+        printIndent("Use: " + whiteGallons +" gallons",CALC_SPC);
+        printIndent("Purchase: " + String.format("%.0f",whiteGallonsPurchase) +" gallons",CALC_SPC);
+        printIndent("Price: $" + whiteCost,CALC_SPC);
+        printIndent("",0);
+        printIndent("Blue @ $30.98/gallon",CALC_SUBHD_SPC);
+        printIndent("Use: " + blueGallons + " gallons",CALC_SPC);
+        printIndent("Purchase: " + String.format("%.0f",blueGallonsPurchase)  + " gallons",CALC_SPC);
+        printIndent("Price: $" + blueCost,CALC_SPC);
+        printIndent("",0);
+        printIndent("TOTAL COST: $" + totalCost + "", CALC_SUBHD_SPC);
+        printIndent("",0);
+    }
+
+}
